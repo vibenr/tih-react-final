3c0fb28848c03e9ae3a01a54a342e636b77f1ea1
kyr1234 <yugkhokhar18@gmail.com>
2022-01-12 23:57:35 +0530

    Old Part

--- src/Components/Redirectform copy/Redirectform.js
+++ src/Components/Redirectform copy/Redirectform.js
@@ -27,7 +27,14 @@ function Redirectform({pageheading,formname}) {
             </div>
         </div>
 
-      
+        <div className="flex flex-wrap -mx-3 mb-2">
+                <div className="w-full md:w-full px-3 mb-6 md:mb-0">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
+                        Upload The Image
+                    </label>
+                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 w-full rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="file" name="file" type="file"/>
+                </div>
+        </div>
 
     <div>
         <button className="btn btn-primary text-white p-2 mt-4" type="submit">Submit</button>
