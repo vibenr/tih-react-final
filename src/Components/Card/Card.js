9510f29ff800803c0b2e3c20156fffcdfda052c3
Vaibhav <vaibhavchauhan2410@gmail.com>
2022-01-14 23:11:47 +0530

    course btn

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -5,12 +5,12 @@ function Card({ heading, source, paragraph,button}) {
 	return (
 		<div className='max-w-sm rounded  shadow-lg  p-4  '>
 			{source && <img className='w-full h-1/2' src={source} alt='Sunset in the mountains' />}
-			<div className='px-6 py-4'>
+			<div className='lg:px-6 lg:py-4'>
 				{heading && <div className='font-bold text-xl mb-2 text-center'>{heading}</div>}
 				{ paragraph && <p className='text-gray-700 text-base mb-4 '>{paragraph}</p>}
 			</div>
-		<div className="flex justify-center align-items-center  grid-cols-1 ">
-		   { button &&	<button className='bg-blue-800 text-white font-semibold p-2 rounded-lg  hover:bg-blue-400 mb-6'>Enroll Now</button>}
+		<div className="flex justify-center align-items-center  ">
+		   { button &&	<button className='bg-blue-800 text-white font-semibold p-2 rounded-lg  hover:bg-blue-400 mb-6 lg:mb-4 '>Enroll Now</button>}
 		</div>
 		
 		</div>
