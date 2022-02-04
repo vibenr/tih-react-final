41758a714b527aff882728b8fc194555089f0bc2
kyr1234 <yugkhokhar18@gmail.com>
2022-02-04 21:21:32 +0530

    Completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -30,14 +30,7 @@ function Header() {
             <Link to="/activities">ACTIVITIES</Link>
             <Link to="/about">ABOUT</Link>
           </Popover.Group>
-          <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <Link
-              to="/login"
-              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-            >
-              Login
-            </Link>
-          </div>
+     
         </div>
       </div>
 
@@ -81,7 +74,7 @@ function Header() {
                 <Link to="/startups">STARTUPS</Link>
                 <Link to="/activities">ACTIVITIES</Link>
                 <Link to="/about">ABOUT</Link>
-                <Link to="/login">LOGIN</Link>
+               
               </div>
             </div>
           </div>
